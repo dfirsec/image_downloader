8d55d3ba8fc9fb4fdbcba3e0dda8eba085cdefe0
dfirsec <12067255+dfirsec@users.noreply.github.com>
2021-03-09 11:11:46 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,101 @@
+# Image Downloader
+
+![Generic badge](https://img.shields.io/badge/python-3.7-blue.svg) [![Twitter](https://img.shields.io/badge/Twitter-@pulsecode-blue.svg)](https://twitter.com/pulsecode)
+
+I created this script as a method to download image files from forums that I visit. However, a few tests have demostrated that the script is generally successful across other "basic" websites -- website coding complexity may pose issues extracting image links.
+
+Basically, the script parses tags that contain potential links to image files and passes the results for downloader. The script is a bit crude, but I will optimize it to increase code efficiency.
+
+Files created by script:
+
+- Folder to store image files that contains:
+  - Downloaded image files
+  - JSON file of hashed image files
+- Text file that includes a record of small images files skipped</li>
+
+## Installation
+
+```text
+git clone https://github.com/dfirsec/image_downloader.git
+cd image_downloader
+pip install -r requirements.txt
+```
+
+## Usage
+
+```console
+    +-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+-+
+    | I m a g e   D o w n l o a d e r |
+    +-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+-+
+    v0.0.1         DFIRSec (@pulsecode)
+
+usage: image_downloader.py [-h] [--sksm] [--max] [--hash] url
+
+positional arguments:
+  url         destination url -- surround url string with double quotes
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --sksm      skip image files smaller than 20kB
+  --max       use max threads for downloading
+  --hash      create json record of hashed image files
+```
+
+### Example Run
+
+```text
+python image_downloader.py "<URL>"
+
+    +-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+-+
+    | I m a g e   D o w n l o a d e r |
+    +-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+-+
+    v0.0.1         DFIRSec (@pulsecode)
+
+2021-03-09 08:26:28 INFO Connecting to: https://<URL>
+2021-03-09 08:26:28 INFO Downloaded: 20190327_174324.jpg
+2021-03-09 08:26:28 INFO Downloaded: 20200309_212330.jpg
+2021-03-09 08:26:29 INFO Downloaded: 20180830_161521.jpg
+2021-03-09 08:26:29 INFO Downloaded: 20190714_1636310.jpg
+2021-03-09 08:26:30 INFO Downloaded: 20180910_120057.jpg
+2021-03-09 08:26:30 INFO Downloaded: 20190714_163640.jpg
+2021-03-09 08:26:30 INFO Downloaded: 20190327_131047.jpg
+```
+
+#### Option to skip image files smaller that 20kB
+
+```text
+python image_downloader.py "<URL>" --sksm
+
+    +-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+-+
+    | I m a g e   D o w n l o a d e r |
+    +-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+-+
+    v0.0.1         DFIRSec (@pulsecode)
+
+2021-03-09 08:53:12 INFO File Exists: 20200312_170237.jpg
+2021-03-09 08:53:12 INFO File Exists: 20190326_170715.jpg
+2021-03-09 08:53:12 INFO Small Image: 1235699.svg
+2021-03-09 08:53:12 INFO Small Image: abcdefg.png
+2021-03-09 08:53:12 INFO Small Image: xyz.png
+2021-03-09 08:53:12 INFO File Exists: 20190714_163629.jpg
+2021-03-09 08:53:12 INFO File Exists: 20200309_170653.jpg
+```
+
+```text
+<<< small_image_files.txt >>>
+
+Small File: <URL>/1235699.svg [3.75 KB]
+Small File: <URL>/abcdefg.png [3.985 KB]
+Small File: <URL>/xyz.png [5.091 KB]
+```
+
+#### Option to create json record of hashed image files
+
+```text
+python image_downloader.py "<URL>" --hash
+```
+
+#### Option to increase threads for faster file retrieval
+
+```text
+python image_downloader.py "<URL>" --max
+```
---
+++ image_downloader.py
@@ -0,0 +1,199 @@
+import argparse
+import hashlib
+import json
+import logging
+import os
+import re
+import shutil
+import sys
+import urllib.request
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+from http.client import responses
+from pathlib import Path
+from urllib.error import URLError
+from urllib.parse import urljoin, urlparse
+
+import coloredlogs
+import requests
+from bs4 import BeautifulSoup
+
+from termcolors import Termcolors
+
+__author__ = "DFIRSec (@pulsecode)"
+__version__ = "v0.0.1"
+__description__ = "Website image downloader"
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level="DEBUG", fmt="%(asctime)s %(levelname)s %(message)s", logger=logger)
+
+# Initialize terminal colors
+tc = Termcolors()
+
+# Base directory path
+parent = Path(__file__).resolve().parent
+
+# keep track of small files not downloaded
+small_files = parent.joinpath("small_image_files.txt")
+open(small_files, "w").close()  # create and close file
+
+
+class FileHashing:
+    def __init__(self):
+        self.hashed_json = Path(dir_setup()).joinpath("hashed_files.json")
+
+    @staticmethod
+    def gethash(filepath, blocksize=65536):
+        hasher = hashlib.md5()
+        with open(filepath, "rb") as f:
+            for chunk in iter(partial(f.read, blocksize), b""):
+                hasher.update(chunk)
+        return hasher.hexdigest()
+
+    def hashfiles(self):
+        if not self.hashed_json.exists():
+            with open(self.hashed_json, "w") as f:
+                f.write(json.dumps({}))
+
+        hashes = {}
+        files = [f for f in Path(dir_setup()).iterdir() if f.is_file() and not f.name.endswith("json")]
+        [hashes.update({f.name: self.gethash(f)}) for f in files]
+
+        with open(self.hashed_json) as f:
+            data = json.load(f)
+        data.update(hashes)
+
+        with open(self.hashed_json, "w") as f:
+            json.dump(data, f, indent=4)
+
+
+class Downloader:
+    def __init__(self, url, sksm=None):
+        self.hashed_json = Path(dir_setup()).joinpath("hashed_files.json")
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36"
+        }
+        self.url = url
+        self.resp = requests.get(url, headers=self.headers)
+        self.parser = urlparse(url)
+        self.skip_small = sksm
+
+    def getlinks(self, url):
+        try:
+            logger.info(f"Connecting to: {tc.fg.cyan}{url}{tc.reset}")
+            soup = BeautifulSoup(self.resp.content, "lxml")
+            self.resp.raise_for_status()
+        except Exception as err:
+            logger.error(f"{self.resp.status_code} {responses[self.resp.status_code]}: {url}")
+            sys.exit(logger.error(err))
+        else:
+            # regex to validate urls
+            regex_url = r"(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=\*]*))"
+
+            # find all potential images sources
+            img_src = ["data-src", "src", "data-fallback-src", "data-srcset", "srcset"]
+            img_links1 = [link.get(src) for src in img_src for link in soup.find_all("img") if link.get(src) != None]
+            img_links2 = [i["href"] for i in (img.find_parent("a") for img in soup.select("a[href] img"))]
+            img_links3 = [link.get("href") for link in soup.find_all("a")]
+            matches = img_links1 + img_links2 + img_links3
+
+            # validate the urls from combined full list
+            links_joined = [urljoin(url, link) for link in matches]
+            valid_url = [match.group(0) for match in re.finditer(regex_url, str(links_joined))]
+            results = list(set(valid_url))  # remove any duplicates from list
+
+            # if no images found
+            if not results:
+                sys.exit(logger.info(f"{tc.fg.yellow}No images available for download{tc.reset}"))
+            else:
+                return results
+
+    def download_file(self, directory, url):
+        try:
+            req = urllib.request.Request(url, None, self.headers)
+        except URLError as e:
+            logger.error(f"{e} {url}")
+        except Exception as err:
+            logger.error(f"Download failed: {str(err)}: {url}")
+        else:
+            img_path = Path(directory).joinpath(Path(url).name)
+
+            with urllib.request.urlopen(req) as resp:
+                img_size = resp.headers["Content-Length"]
+                img_maintype = resp.headers.get_content_maintype()
+                img_subtype = resp.headers.get_content_subtype()
+
+                if img_maintype == "image":
+                    # remove special characters from string and add file extension if missing
+                    special = "?=,:*-"
+                    pattern = "[" + special + "]"
+                    repl_str = re.sub(pattern, "", img_path.name)
+                    add_ext = Path(directory).joinpath(repl_str + "." + img_subtype)
+
+                    if self.skip_small and bool(int(img_size) <= 20000):  # skip imagees smaller than 20 kB
+                        size = round(float(int(img_size) / 1000), 3)
+                        with open(small_files, "a") as f:
+                            f.writelines(f"\nSmall File: {resp.url} [{size} KB]")
+                        logger.info(f"{tc.fg.magenta}Skipped Image: {tc.reset}{img_path.name}")
+
+                    elif img_path.exists() or add_ext.exists():
+                        logger.info(f"{tc.fg.yellow}File Exists: {tc.reset}{img_path.name}")
+
+                    else:
+                        suffix = img_path.suffix.replace(".", "")
+                        if suffix != img_subtype and img_subtype != "svg+xml" and suffix != "jpg":
+                            img_path = add_ext
+
+                        with open(img_path, "wb") as fileobj:
+                            shutil.copyfileobj(resp, fileobj)
+                            logger.info(f"Downloaded: {img_path.name}")
+
+
+def dir_setup():
+    path = parent.joinpath("IMG_Downloads")
+    if not path.exists():
+        path.mkdir()
+    return path
+
+
+def main(url, sksm=None, hashing=None, max_threads=None):
+    fh = FileHashing()
+    downloader = Downloader(url, sksm)
+    download_dir = dir_setup()
+    urls = [u for u in downloader.getlinks(url)]
+
+    # If max_workers is None, it will default to min(32, os.cpu_count() + 4)
+    # Ref: https://docs.python.org/3/library/concurrent.futures.html
+    if max_threads:
+        max_threads = min(32, os.cpu_count() + 4) * 2  # double the default
+    else:
+        max_threads = None
+
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
+        download_func = partial(downloader.download_file, download_dir)
+        executor.map(download_func, urls, timeout=30)
+
+    # Option to hash files
+    if hashing:
+        fh.hashfiles()
+
+
+if __name__ == "__main__":
+    banner = fr"""
+    +-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+-+
+    | I m a g e   D o w n l o a d e r |
+    +-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+-+
+    {__version__}         {__author__}
+    """
+
+    print(f"\033[36m{banner}\033[m")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="destination url -- surround url string with double quotes")
+    parser.add_argument("--sksm", action="store_true", help="skip image files smaller than 20kB")
+    parser.add_argument("--max", action="store_true", help="use max threads for downloading")
+    parser.add_argument("--hash", action="store_true", help="create json record of hashed image files")
+
+    args = parser.parse_args()
+
+    main(args.url, args.sksm, args.hash, args.max)
