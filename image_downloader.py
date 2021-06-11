0574f3cf0857000141baa5e13f147b11fd24abe7
dfirsec <12067255+dfirsec@users.noreply.github.com>
2021-06-11 10:44:08 -0400

    Minor Update

--- image_downloader.py
+++ image_downloader.py
@@ -137,7 +137,7 @@ class Downloader:
         try:
             # check for instance of headers
             bool(resp.headers)
-        except Exception:
+        except:
             pass
         else:
             # split content-type image/jpg, or image/gif, etc.
